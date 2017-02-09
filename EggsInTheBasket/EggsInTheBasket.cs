527f36f39d25ebf83a112bca04f1d9b106d4ee9b
romel <romel@DESKTOP-RU04C60>
2017-02-10 10:18:33 +1300

    Add project files.

---
+++ EggsInTheBasket/EggsInTheBasket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EggsInTheBasket
+{
+    class EggsInTheBasket
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The minimum number of Eggs in the basket given the situation is");
+            for (int numberOfEggs = 1; ; numberOfEggs++) {
+                if ((numberOfEggs % 1==0) && (numberOfEggs % 3==0) && (numberOfEggs % 7 ==0) && (numberOfEggs % 9==0)) {
+                    Console.WriteLine(numberOfEggs);
+                    break;
+                }
+                
+            }
+        }
+    }
+}
